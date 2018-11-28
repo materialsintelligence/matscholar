897d54eeb21adb186d63857987dbd9171f3c033c
vtshitoyan <tsh.vahe@gmail.com>
2018-11-28 13:48:56 -0800

    added close_words

--- matscholar/tests/test_rest.py
+++ matscholar/tests/test_rest.py
@@ -22,6 +22,22 @@ class EmbeddingEngineTest(unittest.TestCase):
                                                                       0.12144004553556442, 0.11942288279533386,
                                                                       0.11764131486415863, 0.11382885277271271])
 
+    def test_close_words(self):
+
+        zt_words = self.r.close_words("thermoelectric figure of merit", top_k=10)
+
+        self.assertEqual(zt_words["original_wordphrase"], "thermoelectric figure of merit")
+        self.assertListEqual(zt_words["sentence"], ['thermoelectric_figure_of_merit'])
+        self.assertListEqual(
+            zt_words["close_words"],
+            ['ZT', 'figure_of_merit_ZT', 'thermoelectric_figure_of_merit_ZT', 'seebeck_coefficient', 'zT',
+             'thermoelectric_figure_-_of_-_merit', 'thermoelectric_power_factor', 'ZT_value', 'ZT_values',
+             'dimensionless_figure_of_merit'])
+
+        npt.assert_array_almost_equal(
+            zt_words["scores"],
+            [0.865725, 0.862376, 0.849371, 0.847059, 0.843266, 0.835109, 0.831514, 0.82924, 0.825901, 0.822744])
+
     def test_mentioned_with(self):
 
         response_dict = self.r.mentioned_with(material="Te3Bi2", words=["thermoelectric", "solar_cell"])
